a72c7414f14ba900f7e8866f68bde2332978dfa9
luisTrivinh0 <luis.trivinho@icloud.com>
2022-02-09 15:32:10 -0300

    Atualização do dia 09/02/2022 às 15:32

--- Jogos/Adivinhe o Número/game.py
+++ Jogos/Adivinhe o Número/game.py
@@ -1,15 +1,37 @@
 import emoji
+import pygame
+import unidecode
 from random import randint
-from playsound import playsound
 
-num = int(randint(1, 5))
-player = int(input("""Você consegue acertar qual número estou pensando?
-Insira um número de 1 a 5 e tente a sorte: """))
 
-if num == player:
-    playsound('acertou.mp3', True)
-    print(emoji.emojize('Não acredito :scream:! Acertou :tada:!!', use_aliases=True))
-else:
-    playsound('erro.mp3', True)
-    print(emoji.emojize('Hahaha :joy:! Errou :sob:! Eu pensei no Nº{}'.format(num), use_aliases=True))
+def game(num, player):
+    if num != player:
+        sfx = pygame.mixer.Sound('erro.wav')
+        pygame.mixer.Sound.play(sfx)
+        print(emoji.emojize('Hahaha :joy:! Errou :sob:! Eu pensei no Nº{}'.format(num), use_aliases=True))
+    else:
+        sfx = pygame.mixer.Sound('acertou.wav')
+        pygame.mixer.Sound.play(sfx)
+        print(emoji.emojize('Não acredito :scream:! Acertou :tada:!!', use_aliases=True))
 
+
+print("Você consegue acertar qual número estou pensando?")
+
+while True:
+    pygame.init()
+    num_game = int(randint(1, 5))
+    num_player = input("\nInsira um número de 1 a 5 e tente a sorte: ").strip()
+    if not num_player.isnumeric():
+        print('\n\nVocê está de sacanagem?')
+    else:
+        num_player = int(num_player)
+        if num_player > 5:
+            print('\nLEIA O ENUNCIADO DE NOVO: ')
+        else:
+            game(num_game, num_player)
+            restart = str(input('\n\nDeseja jogar novamente?'
+                                '\nSe não quiser, é só digitar Não.'
+                                '\nCaso contrário, digite Continuar: ')).strip()
+            if unidecode.unidecode(restart.lower()) == 'nao':
+                print('\nMeus parabéns, arregou!')
+                break
