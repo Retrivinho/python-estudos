772ce325dbbd8530ad3e379826e0ceeecb9b39a1
luisTrivinh0 <luis.trivinho@icloud.com>
2022-02-14 16:45:08 -0300

    Atualização do dia 14/02/2022 às 16:44

---
+++ Exercícios/ex053.py
@@ -0,0 +1,8 @@
+frase = str(input('Frase: ')).strip().lower()
+frase.replace(' ', '')
+if frase[::-1] == frase:
+    print(frase[::-1])
+    print('É palíndromo')
+else:
+    print[::-1]
+    print('Não é palíndromo')
